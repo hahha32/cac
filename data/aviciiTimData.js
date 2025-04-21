50485bd8164be95274a8465aca635774ee5c66ba
kevkevUWU <lik777@newschool.edu>
2025-04-21 12:02:30 -0400

    sdvsdcas

---
+++ data/aviciiTimData.js
@@ -0,0 +1,114 @@
+const data = [
+    { //12
+        index: 1,
+        title: 'Illustration',
+        score: 6,
+        magnitude: 3.5,  
+        magnitude2: 1,  
+        magnitude3: 55,  
+        
+    },
+    { //7
+        index: 2,
+        title: 'Design and applied arts',
+        score: 6,
+        magnitude: 2.3,  
+        magnitude2: 1,  
+        magnitude3: 35,  
+
+    },
+    { //6
+        index: 3,
+        title: 'Psychology',
+        score: 14,
+        magnitude: 2.3,  
+        magnitude2: -1.00,  
+        magnitude3: 20,
+        
+
+    },
+    { //1
+        index: 4,
+        title: 'Industrial and product design',
+        score: 8.,
+        magnitude: 2.9,  
+        magnitude2: 1,  
+        magnitude3: 42,
+    },
+    { //4
+        index: 5,
+        title: 'Jazz/jazz studies',
+        score: 4,
+        magnitude: 2.2,  
+        magnitude2: 2,  
+        magnitude3: 62,
+
+    },
+    { //10
+        index: 6,
+        title: 'Fine and studio arts management',
+        score: 7.5,
+        magnitude: 5.5,  
+        magnitude2: 1,  
+        magnitude3: 38,
+    },
+    { //9
+        index: 7,
+        title: 'Web page, digital/multimedia and information resources design',
+        score: 9.5,
+        magnitude: 7.3,  
+        magnitude2: 1,  
+        magnitude3: 18,
+    },
+    { //11
+        index: 8,
+        title: 'Photography',
+        score: 5.5,
+        magnitude: 2.1,  
+        magnitude2: 1,  
+        magnitude3: 53
+
+    },
+    { //3
+        index: 9,
+        title: 'Liberal arts and sciences/liberal studies',
+        score: 5,
+        magnitude: 2.8,  
+        magnitude2: -1,  
+        magnitude3: 30
+    },
+    { //2
+        index: 10,
+
+        title: 'Fine/studio arts',
+        score: 7.5,
+        magnitude: 2.8,  
+        magnitude2: 1,  
+        magnitude3: 58
+    },
+    { //5
+        index: 11,
+
+        title: 'Mass communication/media studies',
+        score: 11,
+        magnitude: 2.4,  
+        magnitude2: 0,  
+        magnitude3: 28
+    },
+    { //8
+        index: 12,
+
+        title: 'Fashion/apparel design',
+        score: 12,
+        magnitude: 9,  
+        magnitude2: 1,  
+        magnitude3: 48
+    },
+]
+
+const bezierData = [
+[.1, 5, .4, 2.9, .4, 5.6, .4, 2.7],
+[.4, 2.7, 0, 4.1, -.1, 3.9, -.1, 3.3],
+[-.1, 3.3, -.4, 4.9, -.3, 2.3, -.1, 4],
+[-.1, 4, .3, 4.4, -.4, 3, -.7, 6.8]
+]
